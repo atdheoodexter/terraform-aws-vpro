2645a8b9784dba2681fb9d5bda88ae0f88d471d0
atdheoodexter <atdheoo@yahoo.com>
2023-05-12 16:25:29 +0200

    vpc module

---
+++ .terraform/vpc.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.VPC_NAME
+  cidr = var.VPC_CIDR
+  azs = [var.ZONE1, var.ZONE2, var.ZONE3]
+  private_subnets = [var.PRIV_SUB1_CIDR, var.PRIV_SUB2_CIDR, var.PRIV_SUB3_CIDR]
+  public_subnets  = [var.PUB_SUB1_CIDR, var.PUB_SUB2_CIDR, var.PUB_SUB3_CIDR]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_dns_hostnames = true
+  enable_dns_support = true
+}
+
+  tags = {
+    Terraform = "true"
+    Environment = "Prod"
+  }
+  vpc_tags = {
+    Name = var.VPC_NAME
+  }
